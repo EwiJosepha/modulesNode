4f254dc8cecbb10ea846abec3021c1174079b851
ewi josepha <ewijosepha@gmail.com>
2023-12-17 20:42:48 +0100

    loading Esm modules files

---
+++ content/synchronousflow.js
@@ -0,0 +1,8 @@
+const {readFileSync,  writeFileSync } = require("fs")
+console.log("start");
+const first = readFileSync("./content/first.txt", "utf8")
+const second = readFileSync("./content/second.txt", "utf8")
+console.log(`${first}, ${second}`);
+
+writeFileSync("./content/result.txt", `here is the result`, {flag: 'a'})
+console.log("start next");
